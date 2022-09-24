723f30ae85a6d01f063b2896aa22096c99c9d95f
Elena Silchenko <elen.silchenko@gmail.com>
2022-09-24 17:05:11 +0200

    Add component with links

--- work-1-react/src/App.js
+++ work-1-react/src/App.js
@@ -3,6 +3,18 @@ import './App.css'
 function Contents() {
   return (
     <div>
+      <h3>Contents</h3>
+      <p>
+        <a href="#biography">Biography</a><br/>
+        <a href="#education">Education</a><br/>
+        <a href="#case">Court case and the denial of degree</a><br/>
+        <a href="#supercomputing">Supercomputing</a><br/>
+        <a href="mailto:philip@gmail.com">philip@gmail.com</a><br/>
+        <a href="tel:+380440000000">+380440000000</a><br/>
+        Telegram : <a href="https://t.me/login">@login_login_login</a><br/>
+        <a href="#reference">Reference</a><br/>
+        <a href="#links">external links</a><br/>
+      </p>
     </div>
   )
 }
