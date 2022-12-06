80145d68a3199f15dd9903c9c25edd2222e67bce
Elena Silchenko <elen.silchenko@gmail.com>
2022-12-06 20:12:30 +0100

    Add task with matrix array

--- work-1-3-js-arrays/js/index.js
+++ work-1-3-js-arrays/js/index.js
@@ -4,12 +4,15 @@ const arr1 = ['a', 'b', 'c']
 const arr2 = [1, 2, 3]
 
 const newArr =arr1.concat(arr2)
+// const newArr = [...arr1, ...arr2]
 console.log(newArr)
 
 // task 2 - given an array ['a', 'b', 'c']. Add elements 1, 2, 3 to the end.
 
 const arr3 = ['a', 'b', 'c']
 arr3.push(1, 2, 3)
+// const arr = [1, 2, 3]
+// arr3.push(...arr)
 console.log(arr3)
 
 // task 3 - given an array [1, 2, 3]. Make it an array [3, 2, 1].
@@ -55,7 +58,7 @@ console.log(arrNums)
 // task 10 - given an array [3, 4, 1, 2, 7]. Sort it.
 
 const arr4 = [3, 4, 1, 2, 7]
-arr3.sort()
+arr3.sort((a, b) => a - b)
 console.log(arr4)
 
 // task 11 - given an array with elements 'Hello,', 'world!'. Display the phrase "Hello, world!".
@@ -67,19 +70,21 @@ console.log(joinedGreeting)
 // task 12 - given an array ['Hello, 'world!']. It is necessary to write in the zero element of this array the word 'Bye,' (that is, instead of the word 'Hello,' there will be 'Bye,').
 
 let message = ['Hello,', 'world!']
-message.splice(0, 1, 'Bye,')
+// message.splice(0, 1, 'Bye,')
+message[0] = 'Bye,'
 console.log(message.join(' '))
 
 // task 13 - create an array arr with elements 1, 2, 3, 4, 5 in two different ways.
 
 const arr = [1, 2, 3, 4, 5]
 const arr5 = new Array(1, 2, 3, 4, 5)
+// const arr6 = new Array(5).map((_empty, index) => 1 + index)
 console.log(arr5)
 
 // task 14 - Створіть масив arr = ['a', 'b', 'c', 'd'] і за допомогою його виведіть на екран рядок 'a+b, c+d'.
 
 const arr6 = ['a', 'b', 'c', 'd']
-console.log(arr6[0] + arr6[1], arr6[2] + arr6[3])
+console.log(`${arr6[0]} + ${arr6[1]}, ${arr6[2]} + ${arr6[3]}`)
 
 // task 15 - Given a multidimensional array arr7. Print the word 'blue'
 
@@ -88,5 +93,19 @@ const arr7 = {
   'en':['blue', 'red', 'green'],
 }
 console.log(arr7['en'][0])
-console.log(arr7['ua'][0])
+console.log(arr7.en[0])
 
+const matrix = [
+  [11, 12, 13],
+  [21, 22, 23],
+  [31, 32, 33],
+]
+
+let sum = 0;
+for (let i = 0; i < 3; i++) {
+  let row = matrix[i];
+  for (let j = 0; j < 3; j++) {
+    sum += row[j];
+  }
+}
+console.log(sum)
