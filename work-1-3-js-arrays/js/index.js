b21a9b8857ea4eb75e5112c60f5c47d552517d73
Elena Silchenko <elen.silchenko@gmail.com>
2022-12-06 13:42:50 +0100

    Add task with array methods concat()

---
+++ work-1-3-js-arrays/js/index.js
@@ -0,0 +1,7 @@
+// task 1 - two arrays are given: ['a', 'b', 'c'] and [1, 2, 3]. Combine them together.
+
+const arr1 = ["a", "b", "c"]
+const arr2 = [1, 2, 3]
+
+const newArr =arr1.concat(arr2)
+console.log(newArr)
