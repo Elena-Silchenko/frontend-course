169810737b59d67e163c09b46d6891db60bd4911
Elena Silchenko <elen.silchenko@gmail.com>
2022-12-06 13:53:24 +0100

    Add task with array methods push()

--- work-1-3-js-arrays/js/index.js
+++ work-1-3-js-arrays/js/index.js
@@ -1,7 +1,14 @@
 // task 1 - two arrays are given: ['a', 'b', 'c'] and [1, 2, 3]. Combine them together.
 
-const arr1 = ["a", "b", "c"]
+const arr1 = ['a', 'b', 'c']
 const arr2 = [1, 2, 3]
 
 const newArr =arr1.concat(arr2)
 console.log(newArr)
+
+// task 2 - given an array ['a', 'b', 'c']. Add elements 1, 2, 3 to the end.
+
+const arr = ['a', 'b', 'c']
+arr.push(1, 2, 3)
+console.log(arr)
+
