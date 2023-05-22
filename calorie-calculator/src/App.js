314da63417b03106beb0d146e90477c242045012
Elena Silchenko <elen.silchenko@gmail.com>
2023-05-22 11:59:29 +0200

    Add image-bkg and title with button, which opens a modal window

--- calorie-calculator/src/App.js
+++ calorie-calculator/src/App.js
@@ -1,12 +1,60 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import React, { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 function App() {
+  const [modalShow, setModalShow] = useState(false);
   return (
-    <div>
-      test
+    <>
+    <div className="wrapper">
+      <div className="main-container">
+        <div className="main-title">Calorie Calculator</div>
+        <div className="main-btn">
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Start here
+          </Button>
+        </div>
+      </div>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
+    </>
   );
 }
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+} 
 
 export default App;
